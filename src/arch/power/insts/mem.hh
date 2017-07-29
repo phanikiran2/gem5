03af98383361d04cb9c1909ae805fe3f73647f69
Sandipan Das <sandipandas1990@gmail.com>
2017-07-29 16:40:22 +0530

    Power: Fix fixed-point load and store instructions
    This changeset fixes the following load and store instructions as
    a result of the change in register widths:
      * Load Word and Zero (lwz)
      * Load Word and Zero Indexed (lwzx)
      * Load Word and Zero with Update (lwzu)
      * Load Word and Zero with Update Indexed (lwzux)
      * Load Word Algebraic (lwa)
      * Store Word (stw)
      * Store Word Indexed (stwx)
      * Store Word with Update (stwu)
      * Store Word with Update Indexed (stwux)
    
    This also fixes disassembly generation for all of the above.
    
    Change-Id: I313f54d77c217cc029a483d3750b781e44f17dba

--- src/arch/power/insts/mem.hh
+++ src/arch/power/insts/mem.hh
@@ -86,6 +86,47 @@ class MemDispOp : public MemOp
     std::string generateDisassembly(Addr pc, const SymbolTable *symtab) const;
 };
 
+
+/**
+ * Class for memory operations with shifted displacement.
+ */
+class MemDispShiftOp : public MemOp
+{
+  protected:
+
+    int16_t disp;
+
+    /// Constructor
+    MemDispShiftOp(const char *mnem, MachInst _machInst, OpClass __opClass,
+                   StaticInstPtr _eaCompPtr = nullStaticInstPtr,
+                   StaticInstPtr _memAccPtr = nullStaticInstPtr)
+      : MemOp(mnem, _machInst, __opClass, _eaCompPtr, _memAccPtr),
+        disp(sext<14>(machInst.ds))
+    {
+    }
+
+    std::string generateDisassembly(Addr pc, const SymbolTable *symtab) const;
+};
+
+
+/**
+ * Class for memory operations with register indexed addressing.
+ */
+class MemIndexOp : public MemOp
+{
+  protected:
+
+    /// Constructor
+    MemIndexOp(const char *mnem, MachInst _machInst, OpClass __opClass,
+               StaticInstPtr _eaCompPtr = nullStaticInstPtr,
+               StaticInstPtr _memAccPtr = nullStaticInstPtr)
+      : MemOp(mnem, _machInst, __opClass, _eaCompPtr, _memAccPtr)
+    {
+    }
+
+    std::string generateDisassembly(Addr pc, const SymbolTable *symtab) const;
+};
+
 } // namespace PowerISA
 
 #endif //__ARCH_POWER_INSTS_MEM_HH__
